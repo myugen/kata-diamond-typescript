3e4a2aa4684e66cc6e4bb9fff70d92e962af7c73
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-06-12 15:41:54 +0100

    test: change to parameterized test

--- src/diamond-kata.spec.ts
+++ src/diamond-kata.spec.ts
@@ -1,26 +1,23 @@
 import { describe, expect, it, vi } from "vitest"
 import { Printer } from "./printer.js"
 import { DiamondKata } from "./diamond-kata.js"
+import { AlphabetLetter } from "./alphabet.js"
+
+type TestCase = { shape: AlphabetLetter; expected: string }
 
 class MockPrinter implements Printer {
   print = vi.fn()
 }
 describe("DiamondKata", () => {
-  it("should print an A-shaped diamond", () => {
+  it.each<TestCase>([
+    { shape: "A", expected: "A" },
+    { shape: "C", expected: "  A  \n B B \nC   C\n B B \n  A  " },
+  ])("should print a $shape shaped diamond", ({ expected, shape }) => {
     const printer = new MockPrinter()
     const diamondKata = new DiamondKata(printer)
 
-    diamondKata.render("A")
+    diamondKata.render(shape)
 
-    expect(printer.print).toHaveBeenCalledWith("A")
-  })
-
-  it("should print an C-shaped diamond", () => {
-    const printer = new MockPrinter()
-    const diamondKata = new DiamondKata(printer)
-
-    diamondKata.render("C")
-
-    expect(printer.print).toHaveBeenCalledWith("  A  \n B B \nC   C\n B B \n  A  ")
+    expect(printer.print).toHaveBeenCalledWith(expected)
   })
 })
